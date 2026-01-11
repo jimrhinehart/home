5e71d160331507cacc1363f7aecc0ca958b97f26
jimrhinehart <jimrhinehart@gmail.com>
2026-01-10 19:43:51 -0500

    stuff

--- README.md
+++
@@ -1,6 +0,0 @@
-# Jim Rhinehart
-If you've found this site, congrats! It's not really for public consumption, just something I'm messing around with!
-
-I'm a pianist (and stuff) - if you're actually looking for me, my email is jimrhinehart at gmail dot com.
-
-I'm also on Facebook [https://www.facebook.com/jrhinehart] and Instagram [https://www.instagram.com/jim_rhinehart/].
--- _source/assets/js/app.js
+++
@@ -1,8 +0,0 @@
-/* ----------------------------------------------------------------------------
-js entry point
----------------------------------------------------------------------------- */
-import animateOnScroll from './_elements/animate-on-scroll.js';
-import PopoverMenu from './_elements/popover-menu.js';
-import RandomShapes from './_elements/random-shapes.js';
-import SmallDetails from './_elements/small-details.js';
-import ThemePicker from './_elements/theme-picker.js';
